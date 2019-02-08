93d75f3bc64f151a4339a6e3e272c7dc59b77607
Ayana Bando <ayanaflax@yahoo.com>
2019-02-08 19:14:45 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,6 @@ class Song < ActiveRecord::Base
     validates :title, presence: true, uniqueness: true
     validates :artist_name, presence: true
     validates :released, presence: true
+    
+    
 end
