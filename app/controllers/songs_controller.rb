9383d1bad14bf71d7d80b41c1d6ee91721bed7b9
Ayana Bando <ayanaflax@yahoo.com>
2019-02-08 19:08:22 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,7 +7,7 @@ class SongsController < ApplicationController
     @song = Song.new(song_params)
 
     respond_to do |format|
-      if @post.save
+      if @song.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
