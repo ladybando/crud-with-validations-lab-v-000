80d056e88554743f5431cfd0ec4df6bdbc71506f
Ayana Bando <ayanaflax@yahoo.com>
2019-02-08 19:32:27 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -38,7 +38,8 @@ class SongsController < ApplicationController
   end
   
   private
-
+    def set_song
+      @song = Song.find(params[:id])
     # Never trust parameters from the scary internet, only allow the white list through.
     def song_params
       params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
