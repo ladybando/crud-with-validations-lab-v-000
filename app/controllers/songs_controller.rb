3877be38963d9fa2e27444505c50993f8e73c584
Ayana Bando <ayanaflax@yahoo.com>
2019-02-08 19:22:23 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,15 +6,6 @@ class SongsController < ApplicationController
   def create
     @song = Song.new(song_params)
 
-    respond_to do |format|
-      if @song.save
-        format.html { redirect_to @song, notice: 'Song was successfully created.' }
-        format.json { render :show, status: :created, location: @song }
-      else
-        format.html { render :new }
-        format.json { render json: @song.errors, status: :unprocessable_entity }
-      end
-    end
   end
   
   def update
