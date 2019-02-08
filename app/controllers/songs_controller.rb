9276cbee72639576a5bfaf9c31970877b3af49f8
Ayana Bando <ayanaflax@yahoo.com>
2019-02-08 19:08:10 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,7 +4,17 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.create!()
+    @song = Song.new(song_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
   end
   
   def update
@@ -18,12 +28,12 @@ class SongsController < ApplicationController
   
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_post
+    def set_song
       @song = Song.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
+    def song_params
       params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
     end
 
