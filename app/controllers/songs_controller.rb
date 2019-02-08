c2c97a02110a0bafdb366002ffdd38d2c81f0372
Ayana Bando <ayanaflax@yahoo.com>
2019-02-08 19:26:10 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -38,10 +38,6 @@ class SongsController < ApplicationController
   end
   
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_song
-      @song = Song.find(params[:id])
-    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def song_params
