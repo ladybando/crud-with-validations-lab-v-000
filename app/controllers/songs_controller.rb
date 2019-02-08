d1b67214c2d3673afca0343e415e4c23676884c1
Ayana Bando <ayanaflax@yahoo.com>
2019-02-08 19:22:38 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,7 +5,12 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
-    
+        if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
   end
   
   def update
