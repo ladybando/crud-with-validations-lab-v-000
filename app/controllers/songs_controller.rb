d8db4642ce0e69138927dd6795c6f1281886237e
Ayana Bando <ayanaflax@yahoo.com>
2019-02-08 19:06:14 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,6 +17,14 @@ class SongsController < ApplicationController
   end
   
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @song = Song.find(params[:id])
+    end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:song).permit(:title, :)
+    end
 
 end
